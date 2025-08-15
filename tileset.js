4fbe9b487c712be0f870946cd839942c752c2b48
makibaby78 <raindrops.dark@gmail.com>
2025-08-16 05:13:49 +0800

    first commit

---
+++ tileset.js
@@ -0,0 +1,32 @@
+const tilesets = [
+  { src: 'Hills.png', cols: 22, rows: 14, color: '#9bd4c3' }, //0 - 307
+  { src: 'Wooden_House_Roof_top_left.png', cols: 1, rows: 1 }, //308
+  { src: 'Wooden_House_Roof_top_center.png', cols: 1, rows: 1 }, //309
+  { src: 'Wooden_House_Roof_top_right.png', cols: 1, rows: 1 }, //310
+
+  { src: 'Wooden_House_Roof_center_right.png', cols: 1, rows: 1 }, //311
+  { src: 'Wooden_House_Roof_center_left.png', cols: 1, rows: 1 }, //312
+  { src: 'Wooden_House_Roof_center_center.png', cols: 1, rows: 1 }, //313
+
+  { src: 'Wooden_House_Roof_bottom_right.png', cols: 1, rows: 1 }, //314
+  { src: 'Wooden_House_Roof_bottom_left.png', cols: 1, rows: 1 }, //315
+  { src: 'Wooden_House_Roof_bottom_center.png', cols: 1, rows: 1 }, //316
+
+  { src: 'Wooden_House_Walls_tile_right_edge.png', cols: 1, rows: 1 }, //317
+  { src: 'Wooden_House_Walls_tile_left-edge.png', cols: 1, rows: 1 }, //318
+  { src: 'Wooden_House_Walls_tile_center.png', cols: 1, rows: 1 }, //319
+
+  { src: 'Wooden_House_Walls_window.png', cols: 1, rows: 1 }, //320
+
+  { src: 'Wooden_House_Roof_upwards_center_right.png', cols: 1, rows: 1 }, //321
+  { src: 'Wooden_House_Roof_upwards_center_center.png', cols: 1, rows: 1 }, //322
+  { src: 'Wooden_House_Roof_upwards_center_left.png', cols: 1, rows: 1 }, //323
+
+  { src: 'Wooden_House_Roof_downwards_center_left.png', cols: 1, rows: 1 }, //324
+  { src: 'Wooden_House_Roof_downwards_center_center.png', cols: 1, rows: 1 }, //325
+  { src: 'Wooden_House_Roof_downwards_center_right.png', cols: 1, rows: 1 }, //326
+  
+  { src: 'Doors.png', cols: 1, rows: 4 }, // 327 - 330
+  { src: 'water.png', cols: 4, rows: 1 }, // 331 - 334
+  { src: 'Grass.png', cols: 22, rows: 14 },
+];
