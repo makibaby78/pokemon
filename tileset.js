e2f9ac0a40856cdeb9420ed76d4d529d592dc66f
Max Loued Baisac <raindrops.dark@gmail.com>
2025-09-02 13:28:12 +0800

    My changes

---
+++ tileset.js
@@ -0,0 +1,37 @@
+const tilesets = [
+  { src: 'Hills.png', cols: 22, rows: 14 }, //0 - 307
+  { src: 'Wooden_House_Roof_top_left.png', cols: 1, rows: 1 }, //308
+  { src: 'Wooden_House_Roof_top_center.png', cols: 1, rows: 1 }, //309
+  { src: 'Wooden_House_Roof_top_right.png', cols: 1, rows: 1 }, //310
+
+  { src: 'Wooden_House_Roof_center_right.png', cols: 1, rows: 1 }, //311
+  { src: 'Wooden_House_Roof_center_left.png', cols: 1, rows: 1 }, //312
+  { src: 'Wooden_House_Roof_center_center.png', cols: 1, rows: 1 }, //313
+
+  { src: 'Wooden_House_Roof_bottom_right.png', cols: 1, rows: 1 }, //314
+  { src: 'Wooden_House_Roof_bottom_left.png', cols: 1, rows: 1 }, //315
+  { src: 'Wooden_House_Roof_bottom_center.png', cols: 1, rows: 1 }, //316
+
+  { src: 'Wooden_House_Walls_tile_right_edge.png', cols: 1, rows: 1 }, //317
+  { src: 'Wooden_House_Walls_tile_left-edge.png', cols: 1, rows: 1 }, //318
+  { src: 'Wooden_House_Walls_tile_center.png', cols: 1, rows: 1 }, //319
+
+  { src: 'Wooden_House_Walls_window.png', cols: 1, rows: 1 }, //320
+
+  { src: 'Wooden_House_Roof_upwards_center_right.png', cols: 1, rows: 1 }, //321
+  { src: 'Wooden_House_Roof_upwards_center_center.png', cols: 1, rows: 1 }, //322
+  { src: 'Wooden_House_Roof_upwards_center_left.png', cols: 1, rows: 1 }, //323
+
+  { src: 'Wooden_House_Roof_downwards_center_left.png', cols: 1, rows: 1 }, //324
+  { src: 'Wooden_House_Roof_downwards_center_center.png', cols: 1, rows: 1 }, //325
+  { src: 'Wooden_House_Roof_downwards_center_right.png', cols: 1, rows: 1 }, //326
+  
+  { src: 'Doors.png', cols: 1, rows: 4 }, // 327 - 330
+  { src: 'water.png', cols: 4, rows: 1 }, // 331 - 334
+  { src: 'Grass.png', cols: 22, rows: 14 }, // 335 - 642
+  { src: 'Paths.png', cols: 4, rows: 4 }, // 643 - 659
+  { src: 'Basic_Plants.png', cols: 6, rows: 2 }, // 660 - 671
+  { src: 'Basic_Furniture.png', cols: 9, rows: 6 }, // 671 - 724
+  { src: 'Fences.png', cols: 4, rows: 4 }, // 725 - 740
+  { src: 'Wood_Bridge.png', cols: 5, rows: 3 }, // 741 - 756
+];
